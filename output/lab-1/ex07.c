64deb54251beeaca6d166ac5c8e34d83faeaf369
AggaPaing07 <68011861@kmitl.ac.th>
2025-07-08 23:54:49 +0700

    Add files via upload

--- output/lab-1/ex07.c
+++ output/lab-1/ex07.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 int main()
 {
-    printf("\n%5s", "^");
+    printf("\n%8s", "^");
+    printf("%16s", "^");
+    printf("\n%2s", "(");
+    printf("%10s", "^");
+    printf("%18s","^");
+    printf("%20s",")");
+    printf("\n%16s","o\n");
+    
+    return 0;
 }
